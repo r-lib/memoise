7d0245e649681f4b5c5a17057eb0371fb33d09f0
hadley <h.wickham@gmail.com>
2010-09-03 13:40:55 -0500

    First check in of memoise package

---
+++ R/cache.r
@@ -0,0 +1,28 @@
+new_cache <- function() {
+  
+  cache <- NULL
+  cache_reset <- function() {
+    cache <<- new.env(TRUE, emptyenv())
+  }
+  
+  cache_set <- function(key, value) {
+    assign(key, value, env = cache)
+  }
+  
+  cache_get <- function(key) {
+    get(key, env = cache, inherits = FALSE)
+  }
+  
+  cache_has_key <- function(key) {
+    exists(key, env = cache, inherits = FALSE)
+  }
+  
+  cache_reset()
+  list(
+    reset = cache_reset, 
+    set = cache_set, 
+    get = cache_get,
+    has_key = cache_has_key,
+    keys = function() ls(cache)
+  )
+}
