396242767a178b0776dbe98eccef08967b3a51c8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-14 14:18:30 +0200

    Revert "use INLA repo"
    This reverts commit 9ffcd03415b2bdd3af4b8b61956d41b0d7e8638e.
    
    `revdep_check` doesn't respect `options(repos = ...)`

--- revdep/check.R
+++ revdep/check.R
@@ -1,7 +1,5 @@
 library("devtools")
 
-options(repos = c(getOption("repos"), INLA = "http://www.math.ntnu.no/inla/R/stable"))
-
 res <- revdep_check(bioconductor = TRUE)
 revdep_check_save_summary(res)
 revdep_check_save_logs(res)
