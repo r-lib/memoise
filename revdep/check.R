9ffcd03415b2bdd3af4b8b61956d41b0d7e8638e
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-14 13:57:31 +0200

    use INLA repo

--- revdep/check.R
+++ revdep/check.R
@@ -1,5 +1,7 @@
 library("devtools")
 
+options(repos = c(getOption("repos"), INLA = "http://www.math.ntnu.no/inla/R/stable"))
+
 res <- revdep_check(bioconductor = TRUE)
 revdep_check_save_summary(res)
 revdep_check_save_logs(res)
