e31029d5cab40f8893584ab455eb05f095e1efe3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-17 15:12:38 +0200

    add visibility tests for old-style interface

--- tests/testthat/test-memoise.R
+++ tests/testthat/test-memoise.R
@@ -123,4 +123,12 @@ test_that("old-style interface", {
 
   expect_equal(fm(), 1)
   expect_equal(fm(3), 3)
+  expect_true(withVisible(fm())$visible)
+})
+
+test_that("old-style interface with invisible result", {
+  expect_warning(fm <- memoise(f), "old-style")
+  f <- function(a = 1) invisible(a)
+
+  expect_false(withVisible(fm())$visible)
 })
